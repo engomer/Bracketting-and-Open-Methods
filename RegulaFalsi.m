5e3a19c8c8270cf323c57659e81fafea84cca78b
engomer <43203464+engomer@users.noreply.github.com>
2020-05-29 14:41:47 +0300

    Update RegulaFalsi.m

--- RegulaFalsi.m
+++ RegulaFalsi.m
@@ -1,4 +1,4 @@
-function  result = RegulaFalsi(xi,xf,f,index, es,rt)
+function  result = RegulaFalsi(xi,xf,f, es)
 	%% RegulaFalsi function for Regula Falsi Method
 	%% Takes lower bound, upper bound, function, stopping iteration number and stopping approximate relative error percent as parameter
 	%% Tries to find the root of the equation with iterating the x value
@@ -9,11 +9,11 @@ function  result = RegulaFalsi(xi,xf,f,index, es,rt)
     xc = xu - ((f(xu)*(xl-xu))/(f(xl)-f(xu))); %c variable
     xcold = xc; %xcold to hold the previous c value
     ea = 100;
-    et = abs((rt-xc)/rt)*100; %true relative error
-    fprintf("iter       xl      xu      xc      ea      et\n");
-    fprintf("0     %.9f    %.9f    %.9f    %.9f     %.9f\n",  xl, xu, xc, ea, et);
     
-    while(ea>es || iter< index)
+    fprintf("iter       xl      xu      xc      ea      \n");
+    fprintf("0     %.9f    %.9f    %.9f    %.9f     \n",  xl, xu, xc, ea);
+    
+    while(ea>es)
     xcold = xc;
     
     if f(xc) * f(xu) > 0
@@ -26,8 +26,7 @@ function  result = RegulaFalsi(xi,xf,f,index, es,rt)
     xc = xu - ((f(xu)*(xl-xu))/(f(xl)-f(xu)));
 
     ea = abs((xc-xcold)/xc) * 100;
-    et = abs((rt-xc)/rt)*100;
-    fprintf("%d     %.9f    %.9f    %.9f    %.9f    %.9f\n", iter, xl, xu, xc, ea, et);
+    fprintf("%d     %.9f    %.9f    %.9f    %.9f    \n", iter, xl, xu, xc, ea);
     
     
     iter = iter+1;
